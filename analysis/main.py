0760103385e819e35eba56d7473d2fe93592f2af
Harry45 <arrykrish@gmail.com>
2023-08-21 14:07:40 +0100

    KiDS-1000

--- analysis/main.py
+++ analysis/main.py
@@ -11,20 +11,9 @@ import inference.sampling as sm
 # MCMC.posterior_sampling('Experiment_3_chain_24_10000')
 
 
-# MCMC = sm.mcmc('grouping', 'PeeE', True)
-# MCMC.posterior_sampling('Experiment_4_grouping_samples_chain_24_10000')
+MCMC = sm.mcmc('grouping', 'PeeE', True)
+MCMC.posterior_sampling('Experiment_4_grouping_samples_chain_24_10000')
 
-
-import inference.likelihood as lk
-testing = lk.sampling_dist('grouping', 'PeeE', True)
-
-cosmo = {'omega_cdm':0.10802979609861982, 'omega_b': 0.02332493567194329, 'S_8': 0.815371910192108, 'n_s': 0.9704551766630282, 'h': 0.6887657808123486}
-shifts = {'d1': 0.0, 'd2': 0.002, 'd3': 0.013, 'd4': 0.011, 'd5': -0.006}
-nuisa = {'A_bary': 2.5, 'A_IA': 1.0}
-
-print(testing.loglike(cosmo, shifts, nuisa)
-
-      )
 # def run_mcmc(fname):
 #     MCMC = sm.mcmc(nz_method='som', stats='PeeE', save=True)
 #     MCMC.posterior_sampling(fname)
