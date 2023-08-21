425dba278d2ab3bc36dbb82923037053bba35095
Harry45 <arrykrish@gmail.com>
2023-08-21 12:28:43 +0100

    setup

--- analysis/setemu.py
+++ analysis/setemu.py
@@ -75,7 +75,7 @@ nz = 30
 k_pivot = 0.05
 
 # bbn = '/home/harry/Desktop/class/bbn/sBBN.dat'
-bbn = '/mnt/zfsusers/phys2286/class/external/bbn'
+bbn = '/mnt/zfsusers/phys2286/class/external/bbn/sBBN.dat'
 # -----------------------------------------------------------------------------
 # folders and files
 
