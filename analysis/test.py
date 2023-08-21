0ff69263382aa3eb4cd740b5818c2933db7d7907
Harry45 <arrykrish@gmail.com>
2023-08-21 13:36:44 +0100

    testing

--- analysis/test.py
+++ analysis/test.py
@@ -10,7 +10,7 @@ class_args = {'z_max_pk': 2.0,
                         #    'halofit_k_per_decade': se.halofit_k_per_decade,
                         #    'halofit_sigma_precision': se.halofit_sigma_precision,
                         #    'k_pivot': se.k_pivot,
-                        #    'sBBN file': se.bbn
+                           'sBBN file': se.bbn
                            }
 
 
